2954b4ddda60b70d3af8c66095319e8b49c07441
VHPL-UIS <vahidtest80@gmail.com>
2024-11-25 14:17:44 +0100

    from local to git

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,35 @@
+import Game from "../entities/Game";
+import { Card, Image, CardBody, Heading, HStack } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
+import Emoji from "./Emoji";
+import { Link } from "react-router-dom";
+
+interface Props {
+  game: Game;
+}
+
+const GameCard = ({ game }: Props) => {
+  return (
+    <Card>
+      <Link to={`/games/${game.slug}`}>
+        <Image src={getCroppedImageUrl(game.background_image)} />
+        <CardBody>
+          <HStack justifyContent="space-between" marginBlock={3}>
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
+          <Heading fontSize="2xl">
+            {game.name}
+            <Emoji rating={game.rating_top} />
+          </Heading>
+        </CardBody>
+      </Link>
+    </Card>
+  );
+};
+
+export default GameCard;
